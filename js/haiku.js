d9e506b0124ad38e3727f01c019fbae49bf7d6da
jamie pittak and panatda inthala <panatda@>
2018-03-14 16:18:24 -0700

    adds some exceptions to words

--- js/haiku.js
+++ js/haiku.js
@@ -7,32 +7,60 @@ export class Haiku{
     this.lineThree = lineThree.split("");
   }
 
-//   syllableCounter()
-//   {
-//
-//     let syllableCount = 0;
-//
-//     for(i = 0; i < this.length; i++)
-//       if (this.lineOne.split("")includes("ae") || this.lineTwo.split("")includes("ae") || this.lineThree.split("")includes("ae")){
-//         syllableCount -= 1;
-//         return syllableCount;
-//   }
-//
-// }
-}
+  syllableCounter()
+  {
 
-//  export function syllableCounter(arrayOne, arrayTwo, arrayThree){
-//   let firstArray = arrayOne.split("");
-//   let secondArray = arrayTwo.split("");
-//   let thirdArray = arrayThree.split("");
-//     const vowelString = ["a", "e", "i", "o", "u", "y"];
-//
-//     for (var i = 0; i < firstArray.length; i++) {
-//       for (var o = 0; o < vowelString.length; o++) {
-//         if (firstArray[i] == vowelString[o]) {
-//
-//           return firstArray;
-// }
-// }
-// }
-// }
+    let vowelCount = 0;
+    const vowelString = ["a", "e", "i", "o", "u", "y"];
+
+    for(i = 0; i < this.length; i++){
+      for (var o = 0; o < vowelString.length; o++){
+        if(this.lineOne[i] == vowelString[o] || this.lineTwo[i] == vowelString[o] || this.lineThree[i] == vowelString[o])
+        {
+          vowelCount += 1;
+        }
+      }
+    }
+
+    if (this.lineOne includes("ae") || this.lineTwo includes("ae") || this.lineThree includes("ae")){
+      vowelCount -= 1;
+    }
+
+    if (this.lineOne includes("ou") || this.lineTwo includes("ou") || this.lineThree includes("ou")){
+      vowelCount -= 1;
+
+    }
+
+    if (this.lineOne includes("ea") || this.lineTwo includes("ea") || this.lineThree includes("ea")){
+      vowelCount -= 1;
+    }
+
+    if (this.lineOne includes("uo") || this.lineTwo includes("uo") || this.lineThree includes("uo")){
+      vowelCount -= 1;
+    }
+
+    if (this.lineOne includes("ee") || this.lineTwo includes("ee") || this.lineThree includes("ee")){
+      vowelCount -= 1;
+    }
+
+    if (this.lineOne includes("ie") || this.lineTwo includes("ie") || this.lineThree includes("ie")){
+      vowelCount -= 1;
+    }
+
+    if (this.lineOne includes("ue") || this.lineTwo includes("ue") || this.lineThree includes("ue")){
+      vowelCount -= 1;
+    }
+
+    if (this.lineOne includes("ei") || this.lineTwo includes("ei") || this.lineThree includes("ei")){
+      vowelCount -= 1;
+    }
+
+    if (this.lineOne includes("eu") || this.lineTwo includes("eu") || this.lineThree includes("eu")){
+      vowelCount -= 1;
+    }
+
+    if (this.lineOne includes("eo") || this.lineTwo includes("eo") || this.lineThree includes("eo")){
+      vowelCount -= 1;
+    }
+  }
+}
